c3677d5193756c5f54dfee1227d40e7cdbf60c62
xjoxjox <johanna.vehniainen@cs.helsinki.fi>
2016-01-23 01:29:31 +0200

    tehtävät 1-4

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,3 +1,16 @@
 class Beer < ActiveRecord::Base
   belongs_to :brewery
+  has_many :ratings
+
+  def rating
+    ratings.count(beer_id:self.id)
+  end
+
+  def average_rating
+    @yht = 0
+    ratings.where(beer_id:self.id).each do |rate|
+      @yht += rate.score
+    end
+    @yht.to_f / self.rating
+  end
 end
