33659f2375ae5e7812cd925186bdc1584167f405
xjoxjox <johanna.vehniainen@cs.helsinki.fi>
2016-01-30 03:45:05 +0200

    Viikko 3, korjauksia

--- ratebeer/app/models/membership.rb
+++ ratebeer/app/models/membership.rb
@@ -1,11 +1,5 @@
 class Membership < ActiveRecord::Base
   belongs_to :user
   belongs_to :beer_club
-  validate :member_of_club
 
-  def member_of_club
-    if Membership.where(user_id: self.user.id, beer_club_id: self.beer_club.id)
-      errors.add(self.user.username, "already a member of this club.")
-    end
-  end
 end
--- ratebeer/app/models/user.rb
+++ ratebeer/app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   include RatingAverage
   has_many :ratings, dependent: :destroy
   has_many :beers, through: :ratings
-  has_many :memberships, dependent: :destroy
+  has_many :memberships
   has_many :beer_clubs, through: :memberships
   validates :username, uniqueness: true, length: { minimum: 3,
                                                    maximum: 15}
