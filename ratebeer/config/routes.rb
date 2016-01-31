530ee64682118c65736451cfe5f19e343da4fd74
xjoxjox <johanna.vehniainen@cs.helsinki.fi>
2016-01-31 19:06:38 +0200

    Viikko 3, routes

--- ratebeer/config/routes.rb
+++ ratebeer/config/routes.rb
@@ -19,7 +19,7 @@ Rails.application.routes.draw do
 
   resources :ratings, only: [:index, :new, :create, :destroy]
 
-  resource :session, only: [:new, :create, :delete]
+  resource :session, only: [:new, :create, :destroy]
 
   get 'signup', to: 'users#new'
 
