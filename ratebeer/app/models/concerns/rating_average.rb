39ab9343d5a89e89208a78c3b4d59029f1d76b16
xjoxjox <johanna.vehniainen@cs.helsinki.fi>
2016-02-06 17:49:26 +0200

    Korjauksia

--- ratebeer/app/models/concerns/rating_average.rb
+++
@@ -1,9 +0,0 @@
-module RatingAverage
-  extend ActiveSupport::Concern
-  def average_rating
-    return 0 if ratings.empty?
-    ratings.average(:score).round(2)
-  end
-end
-
-#(ratings.map{ |r| r.score }.sum / ratings.count.to_f).round(2)
--- ratebeer/spec/helpers.rb
+++
@@ -1,9 +0,0 @@
-module Helpers
-
-  def sign_in(credentials)
-    visit signin_path
-    fill_in('username', with:credentials[:username])
-    fill_in('password', with:credentials[:password])
-    click_button('Log in')
-  end
-end
