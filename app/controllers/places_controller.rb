f48523876dc9ca224fe301e001700e418a15e006
xjoxjox <johanna.vehniainen@cs.helsinki.fi>
2016-02-11 21:06:11 +0200

    Viikko 5, tehtävät 5-6

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,11 +2,23 @@ class PlacesController < ApplicationController
   def index
   end
 
+  def show
+    @key = ENV['GOOGLE_APIKEY']
+  end
+
   def search
     if params[:city].empty?
       redirect_to places_path, notice: "City can't be empty"
     else
       @places = BeermappingApi.places_in(params[:city])
+      @places.each do |place|
+        session[place.id.to_s+"_name"] = place.name
+        session[place.id.to_s+"_street"] = place.street
+        session[place.id.to_s+"_zip"] = place.zip
+        session[place.id.to_s+"_city"] = place.city
+        session[place.id.to_s+"_country"] = place.country
+        session[place.id.to_s+"_map"] = place.blogmap
+      end
       if @places.empty?
         redirect_to places_path, notice: "No locations in #{params[:city]}"
       else
--- config/routes.rb
+++ config/routes.rb
@@ -31,6 +31,8 @@ Rails.application.routes.draw do
 
   post 'places', to:'places#search'
 
+  resources :places, only:[:index, :show]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
