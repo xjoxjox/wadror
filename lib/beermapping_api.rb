15f382363d3f0b253c9c374dea59cded8a7883b2
xjoxjox <johanna.vehniainen@cs.helsinki.fi>
2016-02-11 22:42:56 +0200

    travis

--- lib/beermapping_api.rb
+++ lib/beermapping_api.rb
@@ -1,8 +1,5 @@
-require 'httparty'
-
 class BeermappingApi
-  include HTTParty
-  
+
   def self.places_in(city)
     city = city.downcase
     Rails.cache.fetch(city, :expires_in => 1.week) { fetch_places_in(city) }
@@ -12,7 +9,7 @@ class BeermappingApi
 
   def self.fetch_places_in(city)
     url = "http://stark-oasis-9187.herokuapp.com/api/"
-    response = HTTParty.get "#{url}#{ERB::Util.url_encode(city)}"
+    response = HTTParty.get("#{url}#{ERB::Util.url_encode(city)}")
     places = response.parsed_response["bmp_locations"]["location"]
 
     return [] if places.is_a?(Hash) and places['id'].nil?
