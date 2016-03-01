cd0a8db895eecf92d368e7bc23c777e9b78b9809
xjoxjox <johanna.vehniainen@cs.helsinki.fi>
2016-03-01 11:53:56 +0200

    Viikko 7, travis

--- config/application.rb
+++ config/application.rb
@@ -3,9 +3,7 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 require 'bootstrap-sass'
 require "sass-rails"
-require 'webmock'
 
-WebMock.allow_net_connect!
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
